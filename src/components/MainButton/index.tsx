93c0ae44bcab9e421e52570e70198c441c56d399
Hasan KAHRAMAN <kahraman.h@outlook.com>
2021-12-10 16:48:35 +0300

    create custom button component

---
+++ src/components/MainButton/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Button } from "@chakra-ui/react";
+
+function MainButton({
+  title,
+  disabled,
+  isBack,
+  func,
+}: {
+  title: string;
+  disabled: boolean;
+  isBack: boolean;
+  func: () => void;
+}) {
+  return (
+    <Button
+      disabled={disabled}
+      w="170px"
+      backgroundColor={isBack ? "#5453E3" : "#4140E5"}
+      color="#fff"
+      borderRadius="4px"
+      h="48px"
+      fontSize="16px"
+      fontWeight="600"
+      onClick={func}
+    >
+      {title}
+    </Button>
+  );
+}
+
+export default React.memo(MainButton);
