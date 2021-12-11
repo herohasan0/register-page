c72adf8598fadadf3c51740fe2cd55bc0cf58c32
Hasan KAHRAMAN <kahraman.h@outlook.com>
2021-12-11 20:47:10 +0300

    add route paths in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Layout from "./components/Layout";
+import { Routes, Route } from "react-router-dom";
+import Step1 from "./pages/Step1";
+import Step2 from "./pages/Step2";
+import Step3 from "./pages/Step3";
+import Step4 from "./pages/Step4";
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return (
+    <>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Step1 />} />
+          <Route path="select-days" element={<Step2 />} />
+          <Route path="select-days/select-goal" element={<Step3 />} />
+          <Route path="select-days/select-goal/final" element={<Step4 />} />
+        </Routes>
+      </Layout>
+    </>
+  );
 }
 
 export default App;
