3946338d92591a1dc38748ed17d6c1a98abd5f27
Hasan KAHRAMAN <kahraman.h@outlook.com>
2021-12-11 17:33:50 +0300

    edit option component

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -1,28 +1,47 @@
 import React from "react";
-import { Flex, Text, Divider } from "@chakra-ui/react";
-import { CheckIcon } from "@chakra-ui/icons";
 
-function Option({ Title, Checked }: { Title: string; Checked: boolean }) {
+import "../../App.css";
+
+import { CheckIcon } from "@chakra-ui/icons";
+import { Flex, Text, Checkbox } from "@chakra-ui/react";
+
+function Option({
+  Title,
+  Checked,
+  register,
+  required,
+}: {
+  Title?: string;
+  Checked?: boolean;
+  register?: any;
+  required?: any;
+}) {
   return (
-    <Flex
-      w="375px"
-      h="49px"
-      backgroundColor="#FFF"
-      justifyContent="space-between"
-      alignItems="center"
-      borderBottom="1px solid #9B9BEB"
-      paddingRight="20px"
-      paddingLeft="20px"
-    >
-      <Text
-        color={Checked ? "#183137" : "#C8D0D2"}
-        fontWeight="400"
-        fontSize="16px"
+    <>
+      <Flex
+        w="375px"
+        h="49px"
+        backgroundColor="#FFF"
+        justifyContent="space-between"
+        alignItems="center"
+        borderBottom="1px solid #9B9BEB"
+        paddingRight="20px"
+        paddingLeft="20px"
+        margin="0 !important"
+        className="option"
       >
-        {Title}
-      </Text>
-      <CheckIcon w="17px" h="17px" color={Checked ? "#5453E3" : "#C8D0D2"} />
-    </Flex>
+        <Checkbox {...register("selected-days")} value={Title}>
+          <Text
+            color={Checked ? "#183137" : "#C8D0D2"}
+            fontWeight="400"
+            fontSize="16px"
+          >
+            {Title}
+          </Text>
+        </Checkbox>
+        <CheckIcon w="17px" h="17px" color={Checked ? "#5453E3" : "#C8D0D2"} />
+      </Flex>
+    </>
   );
 }
 
