8eb4a868abbecdaad468633e27f587a622ce70aa
Hasan KAHRAMAN <kahraman.h@outlook.com>
2021-12-16 23:36:33 +0300

    add step 5 page

---
+++ src/pages/Step5/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Flex } from "@chakra-ui/react";
+import { CheckIcon } from "@chakra-ui/icons";
+import Title from "../../components/Title";
+import { useTranslation } from "react-i18next";
+
+function Step5() {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <Flex h="100%" flexDir={"column"} justifyContent={"space-around"}>
+      <Flex flexDir={"column"} alignItems={"center"}>
+        <CheckIcon mb="10px" w={10} h={12} color="green" />
+        <Title Text={t("STEP_5.TITLE")} />
+      </Flex>
+    </Flex>
+  );
+}
+
+export default React.memo(Step5);
