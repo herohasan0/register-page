d80246deb3dd8300ef1317bdd684c05dbc883cb9
Hasan KAHRAMAN <kahraman.h@outlook.com>
2021-12-16 23:36:06 +0300

    add step 5 router

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Step1 from "./pages/Step1";
 import Step2 from "./pages/Step2";
 import Step3 from "./pages/Step3";
 import Step4 from "./pages/Step4";
+import Step5 from "./pages/Step5";
 
 import InfoProvider from "./context/InfoProvider";
 
@@ -19,7 +20,7 @@ function App() {
             <Route path="days" element={<Step2 />} />
             <Route path="days/goal" element={<Step3 />} />
             <Route path="days/goal/register" element={<Step4 />} />
-            <Route path="days/goal/register/complete" element={<Step1 />} />
+            <Route path="days/goal/register/:id" element={<Step5 />} />
           </Routes>
         </InfoProvider>
       </Layout>
